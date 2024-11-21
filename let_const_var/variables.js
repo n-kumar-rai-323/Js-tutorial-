55ef70b3f9cd22422df1e80dd27c5f0ba7a900b6
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-11-21 16:05:04 +0000

    variables

--- let_const_var/variables.js
+++ let_const_var/variables.js
@@ -1,1 +1,5 @@
-co
+const accountID = 3535345
+let accountEmail = "infonkumarrai323@gmail.com"
+var accountPassword = "Nishan234"
+let accountState;
+console.table([accountID, accountEmail,accountPassword, accountState])
