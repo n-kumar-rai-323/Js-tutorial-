ed98f9e3621a9274d03dbc2c7b629bb43dd3b499
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-11-21 16:41:20 +0000

    Datatypes

---
+++ datatypes_ECAM/datatype.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version
+// Number =8798
+// BigInt=
+// String =
+// Boolean=true false
+// null = 
+// undefined=
+// Symbol = unique
+console.log(typeof null) // object
+console.log(undefined) // undefined 
