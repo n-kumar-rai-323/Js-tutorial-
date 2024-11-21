b4e086cdfbc92f383dfa478b44fa8aeab8468be2
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-11-21 16:55:46 +0000

    dType

--- datatypes_ECAM/datatype.js
+++ datatypes_ECAM/datatype.js
@@ -8,3 +8,29 @@
 // Symbol = unique
 console.log(typeof null) // object
 console.log(undefined) // undefined 
+
+
+let score = "33dg"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33"= 33
+// "33sdfa"=Not a Number
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 = ture 0 = false
+// ""=false
+// "nishan"= ture
+
+let somenumber = 33
+let con = String(somenumber)
+console.log(con)
+console.log(typeof con)
