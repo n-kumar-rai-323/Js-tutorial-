8ce111b62481d39f963c461c4bf79163897efb57
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-02 06:46:41 +0000

    String

--- string/string.js
+++ string/string.js
@@ -8,3 +8,12 @@ let str1 = "hello"
 let str2 = " Nishan"
 let str3 = str1 + str2
 console.log(str3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1"  + 2 +2)
+console.log(1 + 2 +"2")
+
+console.log(+true)
+console.log(+"");
