84ce1bb1abb4ff0bf6a74dc0b6491939cd085612
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-02 08:27:31 +0000

    Number and Math

--- string/string.js
+++ string/string.js
@@ -17,3 +17,23 @@ console.log(1 + 2 +"2")
 
 console.log(+true)
 console.log(+"");
+
+
+const gameName = new String('hiteshhc')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length);
+console.log(gameName.toLocaleLowerCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne = "     nishan           "
+console.log(newStringOne.trim())
+
+console.log(gameName.split('-'))
