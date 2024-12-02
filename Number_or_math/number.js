841511235255ab66f2b0bec0ac821a33a89e1419
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-02 09:01:04 +0000

    Number or Math

---
+++ Number_or_math/number.js
@@ -0,0 +1,42 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2))
+
+const otherNumber = 123.6966
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString())
+
+
+// +++++++++++ Maths +++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4))
+
+console.log(Math.round(4.5))
+
+console.log(Math.ceil(4.7))
+
+console.log(Math.floor(4.9));
+
+console.log(Math.random())
+console.log(Math.min(2,3,4,5,6,6))
+console.log(Math.max(4,3,2,6,4))
+
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min +1))+ min)
+
