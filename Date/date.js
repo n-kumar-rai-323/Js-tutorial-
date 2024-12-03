01653a748aebaf85beb9efe6d1c1ef7f9396638f
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-03 08:38:11 +0000

    Date

--- Date/date.js
+++ Date/date.js
@@ -9,6 +9,26 @@ console.log(myDate.toLocaleDateString())
 console.log(myDate.toLocaleString())
 console.log(typeof(myDate))
 
-
-let myCreateDate = new Date(2024,12,14)
+//Date create
+let myCreateDate = new Date("2024-11-14")
 console.log(myCreateDate.toDateString())
+
+//Date timestamp 
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+// console.log(myTimeStamp.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getTime())
+console.log(newDate.getFullYear())
+console.log(newDate.getDay())
+console.log(newDate.getMonth())
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    timeZone: '',
+})
