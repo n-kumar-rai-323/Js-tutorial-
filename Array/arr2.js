b952c538d69394fb6d957dbcf7d4db3d54b03c3e
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-03 09:59:19 +0000

    Array

--- Array/arr2.js
+++ Array/arr2.js
@@ -5,7 +5,7 @@ const categorizesd = ["Stationary", "Eletorinec", "Other_Categorized"]
 // console.log(accessories)
 // console.log(categorizesd)
 
-const allDetail= accessories.concat(categorizesd)
+const allDetail = accessories.concat(categorizesd)
 console.log(allDetail)
 
 const allDetList = [...accessories, ...categorizesd]
@@ -13,14 +13,14 @@ console.log(allDetList)
 
 console.log(Array.isArray("Nishan"))
 console.log(Array.from("Nishan"))
-console.log(Array.from({name:"Nishan"})) //interesting 
+console.log(Array.from({ name: "Nishan" })) //interesting 
 
 
 let socre1 = 100
-let score2= 200
-let score3= 300
-console.log(Array.of(socre1,score2,score3));
+let score2 = 200
+let score3 = 300
+console.log(Array.of(socre1, score2, score3));
 
-const another_array = [1,2,3,[4,5,6], 7,[6,7,[4,5]]]
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 const list = another_array.flat(Infinity)
 console.log(list)
