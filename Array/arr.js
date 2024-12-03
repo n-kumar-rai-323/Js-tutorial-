cd3fbee08fee5e7d43e88fd6100d5e7b66992f26
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-03 09:19:58 +0000

    Array

---
+++ Array/arr.js
@@ -0,0 +1,41 @@
+// Array 
+const myArr = [3,453,5,35,3,5,3,true, "nishan"]
+
+console.log(myArr[4])
+
+// method 
+
+const myherios = ["laptop","Desktop", "iphone"]
+
+myherios.push("Pen")
+console.log(myherios)
+
+myherios.pop()
+console.log(myherios)
+
+myherios.unshift('Earphone')
+console.log(myherios)
+
+myherios.shift()
+console.log(myherios)
+
+console.log(myherios.includes("Desktop"))
+console.log(myherios.indexOf("laptop"))
+
+const newArr = myArr.join()
+console.log(newArr)
+console.log(typeof(newArr))
+
+
+// slice, slice
+let number =[1,2,3,4,5,6,7,8]
+console.log("A", number);
+
+const myn1 = number.slice(1,3)
+console.log(myn1);
+
+console.log("B", number)
+
+const myn2 = number.splice(1,3)
+console.log("C", number)
+console.log(myn2)
