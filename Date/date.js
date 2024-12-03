009aa115680e2ab3e92d04d0a89fc10347852a23
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-03 08:27:31 +0000

    Date

---
+++ Date/date.js
@@ -0,0 +1,14 @@
+///Dates 
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof(myDate))
+
+
+let myCreateDate = new Date(2024,12,14)
+console.log(myCreateDate.toDateString())
