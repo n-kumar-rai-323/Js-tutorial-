d088f2ba1366c97d16e32d353bbecb4af47c1135
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-05 16:00:14 +0000

    object

--- object/object2.js
+++ object/object2.js
@@ -1,29 +1,60 @@
-const tinderUser = new Object()
-console.log(tinderUser)
+// const tinderUser = new Object()
+// console.log(tinderUser)
 
-const tinerusers = {}
+// const tinerusers = {}
 
-tinerusers.id = "123abc"
-tinerusers.name = "nishan"
-tinerusers.isLoggedIn = false
+// tinerusers.id = "123abc"
+// tinerusers.name = "nishan"
+// tinerusers.isLoggedIn = false
 
 
-const regularUser = {
-    email:"some@gmail.com",
-    fullname:{
-        userfullname:{
-            firsname : "Nishan",
-            lastname: "Rai"
-        }
+// const regularUser = {
+//     email:"some@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firsname : "Nishan",
+//             lastname: "Rai"
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullname.userfullname.firsname)
+
+
+// const obj1 ={1: "a", 2:"b"}
+// const obj2 = {3: "a", 4:"b"}
+
+// const obj3=Object.assign({},obj1,obj2)
+
+// console.log(obj3)
+
+// const obj4 ={...obj1, ...obj2}
+// console.log(obj4)
+
+
+
+
+
+const users =[
+    {
+        id:1,
+        email: "h@gmail.com"
+    },
+    {
+        id:2,
+        email:"ks@gmail.com"
+    },
+    {
+        id:3,
+        email:"kj@gmail.com"
+    },
+    {
+        id:4,
+        email:"kj@gmail.com"
     }
-}
+]
 
-console.log(regularUser.fullname.userfullname.firsname)
-
-
-const obj1 ={1: "a", 2:"b"}
-const obj2 = {3: "a", 4:"b"}
-
-const obj3=Object.assign({},obj1,obj2)
-
-console.log(obj3)
+users[1].email
+console.log(Object.keys(users))
+console.log(Object.values(users))
+console.log(Object.entries(users))
---
+++ object/object3.js
@@ -0,0 +1,10 @@
+const course ={
+    courseName : "js in hindi",
+    price: "999",
+    courseinstructor: "Nishan"
+}
+
+
+const {courseinstructor} = course
+
+console.log(courseinstructor)
