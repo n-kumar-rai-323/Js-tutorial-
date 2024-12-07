78d49432783e9f1101323f5634e2636eddd7da41
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-07 14:26:11 +0000

    switch

---
+++ Contor_flow/switch.js
@@ -0,0 +1,40 @@
+const month =3
+
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("Aplri")
+        break;
+    case 5:
+        console.log("may")
+        break;
+    case 6:
+        console.log("jun");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("August")
+        break;
+    case 9:
+        console.log("September")
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("november");
+        break;
+    case 12:
+        console.log("december")
+        break;
+}
