475055cb0b803e96be5d5d1138ff55823a5637e3
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-11 12:08:48 +0000

    DOM

---
+++ dom/script.js
@@ -0,0 +1,21 @@
+// document.getElementById('title').id
+// document.getElementById('title').className
+
+// document.getElementById('title').getAttribute('id')
+// document.getElementById('title').getAttribute('class')
+
+// document.getElementById('title').setAttribute('class', 'test')
+
+// title.textContent 
+// title.innerHTML
+// title.innerText
+
+
+
+// document.querySelector('h1')
+// document.querySelectorAll('h1')
+
+
+// const list= document.querySelectorAll('li')
+
+// list[0].style.color='green'
