2258901ca84c68463bc67045c2c31ce89429f8c3
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-27 17:35:42 +0545

    promise

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -1,10 +1,73 @@
-const promiseOne = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        console.log("New promise is successfully ")
-    }, 1000)
-    resolve()
+const promiseOne = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async task is complete")
+        resolve()
+    }, 1000);
+});
+
+promiseOne.then(() => {
+    console.log("Promise Consumed");
+});
+
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async Task Two")
+        resolve()
+    }, 1000);
+}).then(() => {
+    console.log("Async 2 Resolved")
+});
+
+
+const promiseThird = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({ username: "Nishan", Email: "infonkumarrai323@gmail.com" })
+    }, 1000);
+});
+promiseThird.then((user) => {
+    console.log(user)
 })
 
-promiseOne.then(()=>{
-console.log("Promise Consumed")
+
+const promiseFour = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve({ username: "Nishan", password: "1234" })
+        } else {
+            reject("Something went wrong")
+        }
+    }, 1000)
 })
+
+promiseFour.then((user) => {
+    console.log("Promise Four")
+    return user.username
+}).then((myusername) => {
+    console.log(myusername)
+}).catch((e) => {
+    console.log(`The error is ${e}`)
+})
+
+
+const promiseFive = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let error = false;
+        if (!error) {
+            resolve({ username: "Nishan", password: "12345" });
+        } else {
+            reject("The error is ")
+        }
+    }, 1000);
+})
+ 
+
+async function newMtd() {
+    // Simulating an async operation
+    const response = await Promise.resolve("This is the response");
+    console.log(response);
+}
+
+// Call the function
+newMtd();
